8c56de0eed4bc2a2994de0e29dd756c12c211728
tpratyushareddy <tpratyushareddy123@gmail.com>
2025-05-18 23:32:53 +0000

    Addition of numbers

---
+++ addition/add.py
@@ -0,0 +1,11 @@
+import sys
+
+if len(sys.argv) < 2:
+    print("Usage: python add.py <num1> <num2> ... <numN>")
+    sys.exit(1)
+
+try:
+    numbers = [int(arg) for arg in sys.argv[1:]]
+    print("Sum is:", sum(numbers))
+except ValueError:
+    print("Please provide only integers.")
