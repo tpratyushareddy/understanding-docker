3f77a61df0c5a8ed6ce1a6936e3038ad79cc7169
tpratyushareddy <tpratyushareddy123@gmail.com>
2025-05-19 00:54:04 +0000

    testing docker

--- addition/add.py
+++ addition/add.py
@@ -1,11 +1,13 @@
-import sys
+#import sys
 
-if len(sys.argv) < 2:
-    print("Usage: python add.py <num1> <num2> ... <numN>")
-    sys.exit(1)
+#if len(sys.argv) < 2:
+ #   print("Usage: python add.py <num1> <num2> ... <numN>")
+  #  sys.exit(1)
 
-try:
-    numbers = [int(arg) for arg in sys.argv[1:]]
-    print("Sum is:", sum(numbers))
-except ValueError:
-    print("Please provide only integers.")
+#try:
+ #   numbers = [int(arg) for arg in sys.argv[1:]]
+  #  print("Sum is:", sum(numbers))
+#except ValueError:
+ #   print("Please provide only integers.")
+
+print("Hello People")
